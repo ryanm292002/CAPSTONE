251003a88f50939f4c0beee53722d67c03443d91
ryanm292002 <71150667+ryanm292002@users.noreply.github.com>
2024-04-01 22:43:20 -0400

    Update safebrowse.py

--- workingCODE/safebrowse.py
+++ workingCODE/safebrowse.py
@@ -1,5 +1,5 @@
 from pysafebrowsing import SafeBrowsing
-dakey = SafeBrowsing('AIzaSyAoY1tC4xBGny1DzE4jhZsez4UXiOnLf5M')
+dakey = SafeBrowsing('apikey')
 daurl = dakey.lookup_urls(['http://malware.testing.google.test/testing/malware/'])
 
 for url, info in daurl.items():
